0ade532f0abdb8f0a93849acc77c05a11add91b5
kamranaar <kamrankhanaar@gmail.com>
2024-04-18 21:50:50 +0530

    week1

---
+++ Counter.java
@@ -0,0 +1,17 @@
+
+public class Counter {
+    int count;
+    Counter()
+    {
+        count=0;
+    }
+    void incr()
+    {       
+        count++;
+    }
+    int show()
+    {
+        return count;
+    }
+
+}
--- Distance.java
+++ Distance.java
@@ -3,6 +3,10 @@ import java.util.Scanner;
 public class Distance {
     float _inch;
     float _feet;
+    Distance(float inch,float feet){
+        _inch=inch;
+        _feet=feet;
+    }
     void set_distance(float inch, float feet)
     {
         _inch = inch;
@@ -12,13 +16,20 @@ public class Distance {
     void get_diatance()
     {
         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter feet ");
         _feet= sc.nextFloat();
+        System.out.println("Enter inch ");
         _inch= sc.nextFloat();
 
     }
     void show_distance()
     {
-        System.out.println(_inch);
-        System.out.println(_feet);
+        System.out.println(_inch +" "+ _feet);
+        //System.out.println(_feet);
+    }
+    void add(Distance d1, Distance d2)
+    {
+        _feet = d1._feet+d2._feet;
+        _inch = d1._inch + d2._inch;
     }
 }
